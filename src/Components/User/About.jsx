7011a2d660dd61310ab8aaaa7a8db2a7a25b12f7
AbdulMoiz8994 <moiza8994@gmail.com>
2021-03-28 12:38:55 +0500

    update About.jsx

--- src/Components/User/About.jsx
+++ src/Components/User/About.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
 
+
 export const About = () => {
+    const useStyle = {
+        textAlign: "center",
+        h1: {
+            fontSize: '30px'
+        }
+
+    }
     return (
-        <div>
-            <h1>hello About page</h1>
+        <div style={useStyle}>
+            <h1 style={useStyle.h1}>My name is Abdul Moiz Batch(7).This is my <a target="_blank" rel="noreferrer" href="https://github.com/AbdulMoiz8994/mern_functional">GitHub Repository.</a> </h1>
         </div>
     )
 }
